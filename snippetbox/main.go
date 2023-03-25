f57c4ffe0c892b43b903e70c52088600839af8fc
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2023-03-26 01:04:42 +0530

    fix routes

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -31,8 +31,8 @@ func main()  {
 	// servermux = router
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
-	mux.HandleFunc("/view", snippetView)
-	mux.HandleFunc("/create", snippetCreate)
+	mux.HandleFunc("/snippet/view", snippetView)
+	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Use the http.ListenAndServe() function to start a new web server
 	log.Print("Starting server on :4000")
