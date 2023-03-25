c463aeefe35369e8ea8ada49edaa6170e62559c8
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2023-03-26 00:56:52 +0530

    added two routes: create and view

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -11,10 +11,22 @@ func home(w http.ResponseWriter, r *http.Request) {
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
+// Add a snippetView handler function.
+func snippetView(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Display a specific snippet..."))
+}
+
+// Add a snippetCreate handler function.
+func snippetCreate(w http.ResponseWriter, r *http.Request)  {
+	w.Write([]byte("Create a new snippet..."))
+}
+
 func main()  {
 	// servermux = router
 	mux := http.NewServeMux()
 	mux.HandleFunc("/", home)
+	mux.HandleFunc("/view", snippetView)
+	mux.HandleFunc("/create", snippetCreate)
 
 	// Use the http.ListenAndServe() function to start a new web server
 	log.Print("Starting server on :4000")
