9e29986d4dab52cfee3681805b2749999351a538
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2023-03-26 01:03:03 +0530

    added condition to home func if url path enter is wrong then 404 error will display

--- snippetbox/main.go
+++ snippetbox/main.go
@@ -8,6 +8,12 @@ import (
 // Handler
 
 func home(w http.ResponseWriter, r *http.Request) {
+
+	if r.URL.Path != "/" {
+        http.NotFound(w, r)
+        return
+    }
+
 	w.Write([]byte("Hello from Snippetbox"))
 }
 
