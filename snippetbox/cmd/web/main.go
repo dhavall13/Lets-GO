ed8b99e2f3e7d8e5ef553e163edd11614be4f960
Dhaval Chaudhari <dhavalchaudhari39@gmail.com>
2023-03-28 23:52:42 +0530

    create logger for info and err message

--- snippetbox/cmd/web/main.go
+++ snippetbox/cmd/web/main.go
@@ -4,6 +4,7 @@ import (
 	"flag"
 	"log"
 	"net/http"
+	"os"
 )
 
 
@@ -12,7 +13,15 @@ func main()  {
 
 	addr := flag.String("addr", ":4000", "HTTP network address")
 
+
 	flag.Parse()
+
+	// log.New to create logger for writing messages in cmd
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+
 	// servermux = router
 	mux := http.NewServeMux()
 
@@ -26,7 +35,7 @@ func main()  {
 	mux.HandleFunc("/snippet/create", snippetCreate)
 
 	// Use the http.ListenAndServe() function to start a new web server
-	log.Printf("Starting server on %s", *addr) // information message
+	infoLog.Printf("Starting server on %s", *addr) // information message
 	err := http.ListenAndServe(*addr, mux)
-	log.Fatal(err) // err message
+	errorLog.Fatal(err) // err message
 }
